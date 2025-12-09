cbf388ed82b506875934c55f66a53c1d05086d6a
SuriyaSR <suriyasun19@gmail.com>
2025-12-10 01:03:33 +0530

    replacing localhost/environment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js"
 import pollRoutes from "./routes/pollRoutes.js"
 
+const PORT = process.env.PORT || 5000;
+
 dotenv.config();
 connectDB();
 
@@ -31,4 +33,4 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log("Server running on port 5000"));
